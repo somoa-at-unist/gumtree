96f2bd28fb0afbdd39d14975725b4064565f6b54
hsh814 <hsh814@naver.com>
2021-01-18 14:53:57 +0900

    Add JdtTreeMapping.java, edit build.gradle.

---
+++ gen.jdt/src/main/java/com/github/gumtreediff/gen/jdt/JdtTreeMapping.java
@@ -0,0 +1,78 @@
+package com.github.gumtreediff.gen.jdt;
+
+import com.github.gumtreediff.matchers.MappingStore;
+import com.github.gumtreediff.matchers.Matcher;
+import com.github.gumtreediff.matchers.Matchers;
+import com.github.gumtreediff.tree.Tree;
+import com.github.gumtreediff.tree.TreeVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class JdtTreeMapping {
+    public JdtTreeMapping() {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("gumtree main");
+        String srcFilename = "org/joda/time/Partial.java";
+        String dstFilename = "org/joda/time/Partial.java";
+        Path srcFile = FileSystems.getDefault().getPath("/home/plase1/Docker/poracle/modules/JQF/src/test/resources/patches/Patch180/Time4b/src/main/java",srcFilename);
+        Path dstFile = FileSystems.getDefault().getPath("/home/plase1/Docker/poracle/modules/JQF/src/test/resources/patches/Patch180/Time4p/src/main/java",dstFilename);
+
+        //Path srcFile = FileSystems.getDefault().getPath("gen.jdt","src", "test", "resources", "simple", "Example_v0.java");
+        //Path dstFile = FileSystems.getDefault().getPath("gen.jdt","src", "test", "resources", "simple", "Example_v1.java");
+        try {
+            (new JdtTreeMapping()).mapping(srcFile, dstFile, srcFilename);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public HashMap<String, Integer> mapping(Path srcFile, Path dstFile, String srcFilename) throws IOException {
+        System.out.println("mapping!!!!");
+        File f = (new File(srcFile.toString()));
+        if(f.exists()) {
+            System.out.println("exists");
+            System.out.println(f.getAbsolutePath());
+        } else {
+            System.out.println("does not exist");
+            System.out.println(f.getAbsolutePath());
+        }
+        JdtTreeContext srcTreeCtxt = (JdtTreeContext) new JdtTreeGenerator().generateFrom().file(srcFile);
+        JdtTreeContext dstTreeCtxt = (JdtTreeContext) new JdtTreeGenerator().generateFrom().file(dstFile);
+
+        final Tree srcRoot = srcTreeCtxt.getRoot();
+        final Tree dstRoot = dstTreeCtxt.getRoot();
+
+        final CompilationUnit srcRootASTNode = (CompilationUnit) srcTreeCtxt.getRootNode();
+        final CompilationUnit dstRootASTNode = (CompilationUnit) dstTreeCtxt.getRootNode();
+
+        Matcher defaultMatcher = Matchers.getInstance().getMatcher();
+        final MappingStore mappings = defaultMatcher.match(srcRoot, dstRoot);
+        //System.out.println(mappings.toString());
+        HashMap<String, Integer> lineMapping = new HashMap<>();
+        TreeVisitor.visitTree(srcRoot, new TreeVisitor(){
+            @Override
+            public void startTree(Tree tree) {
+                int srcLine = srcRootASTNode.getLineNumber(tree.getPos());
+                Tree matching = mappings.getDstForSrc(tree);
+                if(matching != null) {
+                    //System.out.println("tree: " + tree.toString() + " -> " + matching.toString());
+                    int dstLine = dstRootASTNode.getLineNumber(matching.getPos());
+                    //System.out.println(srcLine + " : " + dstLine);
+                    String key = srcFilename + ":" + srcLine;
+                    lineMapping.put(key, dstLine);
+                }
+            }
+
+            @Override
+            public void endTree(Tree tree) {
+            }
+        });
+        return lineMapping;
+    }
+}
